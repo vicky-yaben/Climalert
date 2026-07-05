3d6de77541c94a24a873c28f8abb2385e65941bb
vicky-yaben <myabenacosta@frba.utn.edu.ar>
2026-07-05 18:45:14 -0300

    service clima

---
+++ src/main/java/com/example/demo/services/ClimaServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.services.impl;
+
+import com.example.demo.entities.Clima;
+import com.example.demo.repositories.ClimaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClimaServiceImpl implements ClimaService {
+
+    private final ClimaRepository climaRepository;
+
+    public ClimaServiceImpl(ClimaRepository climaRepository) {
+        this.climaRepository = climaRepository;
+    }
+
+    @Override
+    public Clima guardarClima(Clima clima) {
+        return climaRepository.guardar(clima);
+    }
+
+    @Override
+    public Optional<Clima> obtenerUltimoClima() {
+        return climaRepository.buscarUltimo();
+    }
+
+    @Override
+    public List<Clima> obtenerHistorial() {
+        return climaRepository.buscarTodos();
+    }
+}
---
+++ src/main/java/com/example/demo/services/impl/ClimaService.java
@@ -0,0 +1,15 @@
+package com.example.demo.services.impl;
+
+import com.example.demo.entities.Clima;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClimaService {
+
+    Clima guardarClima(Clima clima);
+
+    Optional<Clima> obtenerUltimoClima();
+
+    List<Clima> obtenerHistorial();
+}
